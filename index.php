3a3b8dca9bb6e37cd1317339a28f0e7798fcd4b0
Mario zelaya <mariozelaya23@gmail.com>
2023-06-20 15:10:43 -0700

    adding sweet alert code scripts and php session variables to index.php

--- index.php
+++ index.php
@@ -30,7 +30,11 @@
       //Login to Admin Dashboard
       if($row['useremail'] == $useremail && $row['userpassword'] == $userpass && $row['role'] == 'Admin'){
 
-      echo $success = "You are logged as Admin";
+      //echo $success = "You are logged as Admin"; this is old login message
+
+      //creating session variables to be use with sweetalerts
+      $_SESSION['status'] = 'You are logged as Admin'; //message
+      $_SESSION['status_code'] = 'success'; //bootstrap class message, this can be success, warning, etc
 
       //redirecting the user to the Admin dashboard
       header("refresh: 1; ui/dashboard.php");
@@ -47,7 +51,11 @@
         //Login to User Dashboard
         if($row['useremail'] == $useremail && $row['userpassword'] == $userpass && $row['role'] == 'User'){
 
-          echo $success = "You are logged in as User";
+          //echo $success = "You are logged in as User"; this is old login message
+
+          //creating session variables to be use with sweetalerts
+          $_SESSION['status'] = 'You are logged in as User'; //message
+          $_SESSION['status_code'] = 'success'; //bootstrap class message, this can be success, warning, etc
 
           //redirecting the user to the User dashboard
           header("refresh: 1; ui/user.php");
@@ -64,7 +72,10 @@
 
     }else{
 
-      echo $error = "Incorrect email or password";
+      //echo $error = "Incorrect email or password";
+      //creating session variables to be use with sweetalerts
+      $_SESSION['status'] = 'Wrong Email or Password or Fields are empty'; //message
+      $_SESSION['status_code'] = 'error'; //bootstrap class message, this can be success, warning, etc
     }
 
   }
@@ -84,6 +95,10 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <!-- Toastr -->
+  <link rel="stylesheet" href="plugins/toastr/toastr.min.css">
   <!-- icheck bootstrap -->
   <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
@@ -146,7 +161,44 @@
 <script src="plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- SweetAlert2 -->
+<script src="plugins/sweetalert2/sweetalert2.min.js"></script>
+<!-- Toastr -->
+<script src="plugins/toastr/toastr.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
 </body>
 </html>
+
+<?php
+
+  if(isset($_SESSION['status']) && $_SESSION['status']!='')
+  {
+
+?><!-- Closing first php -->
+
+  <script>
+
+    $(function() {
+      var Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 4000
+      });
+
+      Toast.fire({
+        icon: '<?php echo $_SESSION['status_code'];?>',
+        title: '<?php echo $_SESSION['status'];?>'
+      })
+
+    });
+
+  </script>
+
+<?php
+
+unset($_SESSION['status']);
+  }
+
+?><!-- Closing second php -->
